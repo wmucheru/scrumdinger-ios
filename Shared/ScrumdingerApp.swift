2d7bb717940da104cd49451acb0f46239da66b4e
William Mucheru <willyk99@gmail.com>
2021-08-12 23:48:53 +0300

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by William Mucheru on 12/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
